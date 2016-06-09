0dee2e40c006223fd42dc6fea9dcf958588567eb
mhakan <m.hakan@live.com>
2016-06-09 12:10:32 +0300

    Çalışan Hali v1

--- MapReduce/MapReduce/Program.cs
+++ MapReduce/MapReduce/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Collections;
 using System.Data;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace MapReduce
 {
@@ -28,37 +30,59 @@ namespace MapReduce
             {
                 al_icerik[i] = new ArrayList();
             }
+            object obj = new object();
 
             #region Verileri Al
-            for (int i = 1; i < dosyaSayisi+1; i++)
+            Console.WriteLine("1. ADIM: Dosya iceriklerinin alınması");
+            Parallel.For(1, dosyaSayisi + 1, (i) =>
             {
                 tamDosya = "../../" + dosya + i.ToString();
-                al_icerik[i-1] = DosyaOku(tamDosya);
-                
-                Console.WriteLine("\nFile{0} Icerigi:\n",i);
-                Console.Read();
-                for (int j = 0; j < al_icerik[i-1].Count; j++)
+
+                al_icerik[i - 1] = DosyaOku(tamDosya);
+                lock (obj)
                 {
-                    Console.WriteLine("{0}",al_icerik[i-1][j]);
+                    Console.WriteLine("\nFile{0} Icerigi:\n", i);
+                    for (int j = 0; j < al_icerik[i - 1].Count; j++)
+                    {
+                        Console.WriteLine("{0}", al_icerik[i - 1][j]);
+                    }
+                }
+            }
+           );
+            #endregion
+
+
+            ConcurrentDictionary<string, int> tumElemanlar = new ConcurrentDictionary<string, int>();
+            ConcurrentDictionary<string, int>[] Elemanlar = new ConcurrentDictionary<string, int>[dosyaSayisi];
+            for (int i = 0; i < dosyaSayisi; i++)
+            {
+                Elemanlar[i] = new ConcurrentDictionary<string, int>();
+            }
+        
+
+            #region Anahtar Değer Çiftleri Oluştur
+            for (int i = 0; i < dosyaSayisi; i++)
+            {
+                //Paralel Arama 
+                Elemanlar[i] = VerileriBelirle(al_icerik[i]);
+                //Ekrana Yazdır
+                Console.WriteLine("\n{0}. Dosya\n", i + 1);
+                foreach (var item in Elemanlar[i])
+                { 
+                    Console.WriteLine("{0}", item); 
                 }
             }
             #endregion
             
-            DataTable []dt = new DataTable[dosyaSayisi];
-            for (int i = 0; i < dosyaSayisi; i++)
-            {
-                dt[i] = new DataTable();
-            }
-            #region Anahtar Değer Çiftleri Oluştur
-            for (int i = 0; i < dosyaSayisi; i++)
-            {
-                dt[i] = VerileriBelirle(al_icerik[i]);
-            }
-            #endregion
 
+            ConcurrentDictionary<string, int> sonuclar = VerileriBirlestir(Elemanlar,dosyaSayisi);
+            Console.WriteLine("\n\nSONUCLAR------\n"); 
+            foreach (var item in sonuclar) 
+            {
+                Console.WriteLine("{0}", item); 
+            }
 
-            //Reduce: Anahtar değer çiftlerini oluştur ve birleştir
-            
+            Console.ReadLine();
             //-------------------
         }
 
@@ -84,40 +108,70 @@ namespace MapReduce
             return al_Satirlar;
         }
 
-        private static DataTable VerileriBelirle(ArrayList al_tumIcerik)
+        private static ConcurrentDictionary<string, int> VerileriBelirle(ArrayList al_tumIcerik)
         {
-            DataTable veriTablosu = new DataTable();
-            veriTablosu.Columns.Add("VeriAdi",typeof(string));
-            veriTablosu.Columns.Add("VeriSayisi", typeof(int));
-
+            ConcurrentDictionary<string, int> Eleman = new ConcurrentDictionary<string, int>();
+            object ob = new object();
             for(int k = 0; k<al_tumIcerik.Count;k++) 
             {
-                int eslesme = 0;
-                int sayi = 1;
-                int veriBoyu = veriTablosu.Rows.Count;
                 string item = al_tumIcerik[k].ToString();
-
-                for (int i = 0; i < veriBoyu; i++)
-                {
-                    string str = veriTablosu.Rows[i]["VeriAdi"].ToString();
-                    if (str == item.ToString())
+                int topEs=0;
+                
+                #region Paralel Arama işlemi
+                Parallel.For(0, al_tumIcerik.Count,
+                    () => 0,
+                    (i, state, eslesme) =>
                     {
-                        eslesme += 1;
-                        sayi = Convert.ToInt32(veriTablosu.Rows[i]["VeriSayisi"]);
-                        sayi = sayi + 1;
-                        veriTablosu.Rows[i]["VeriSayisi"] = sayi;
-                    }   
-                }
+                        if (item == al_tumIcerik[i].ToString())
+                            eslesme++;
+                        return eslesme;
+                    },eslesme=>{
+                        Interlocked.Add(ref topEs, eslesme);
+                    });
+                #endregion
 
-                if (eslesme == 0)
-                {
-                    veriTablosu.Rows.Add(item, sayi);
-                }
-               
+                Eleman.TryAdd(item, topEs);
+                    topEs=0;
             }
-            return veriTablosu;
+
+            return Eleman;
         }
 
+        private static ConcurrentDictionary<string, int> VerileriBirlestir(ConcurrentDictionary<string, int>[] Elemanlar, int dosyaSayi) 
+        {
+            ConcurrentDictionary<string, int> birlestirilmis = new ConcurrentDictionary<string,int>();
+            int eslesme = 0;
+            for (int i = 0; i < dosyaSayi; i++)
+            {
+                Parallel.ForEach(Elemanlar[i], item => 
+                {
+                        int top = 0, val = 0;
+                        string key = "";
+                        Parallel.ForEach(birlestirilmis, item2 =>
+                        {
+                            if (item.Key == item2.Key)
+                            {
+                                eslesme++;
+                                top = item.Value + item2.Value;
+                                val = item2.Value;
+                                key = item2.Key;
+
+                            }
+                        });
+                        if (eslesme == 0)
+                        {
+                            birlestirilmis.TryAdd(item.Key, item.Value);
+                        }
+                        else
+                        {
+                            birlestirilmis.TryUpdate(key, top, val);
+                        }
+                });
+                
+            }
+
+            return birlestirilmis;
+        }
 
     }
 }
